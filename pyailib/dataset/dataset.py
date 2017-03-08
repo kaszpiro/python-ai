3d0b3e585acb5b7d596436f230e3f33f3955a298
kropiak <kropiak@duit.pl>
2017-03-08 12:14:55 +0100

    Przed wypchaniem do public repo

--- pyailib/dataset/dataset.py
+++ pyailib/dataset/dataset.py
@@ -9,11 +9,12 @@ głównym zadaniem tego modułu będzie:
 """
 
 import numpy as np
+from io import StringIO
 
 
 class DataSet:
-    rawData = np.array
-    dataSetMeta = np.array
+    rawData = []
+    dataSetNames = []
     objectCount = int
     attributeCount = int
 
@@ -22,11 +23,22 @@ class DataSet:
         # wydaje się, że powinny być co najmniej dwa sposoby, jeden poprzez wczytanie z pliku a drugi z tablicy
         # np. jako część dataSet-u
         self.load_data_from_file(file, separator)
-        pass
 
     def load_data_from_file(self, file, separator):
-        self.rawData = np.genfromtxt(file, delimiter=separator)
-        pass
+        # otwarcie pliku i odczytanie pierwszej linii
+        with open(file, 'r') as f:
+            first_line = f.readline()
+        self.dataSetNames = np.loadtxt(StringIO(first_line), dtype=bytes, delimiter=separator).astype(str)
+        self.rawData = np.genfromtxt(file, delimiter=separator, skip_header=1)
+        self.objectCount = self.rawData.shape[0]
+        self.attributeCount = self.rawData.shape[1] - 1
 
     def get_part_of_data(self, start, end):
-        pass
+        return self.rawData[start:end, :]
+
+    def get_total_number_of_classes(self):
+        return np.unique(self.rawData[:, self.rawData.shape[1] - 1:self.rawData.shape[1]]).size
+
+    def get_number_of_elements_in_classes(self):
+        unique, counts = np.unique(self.rawData[:, self.rawData.shape[1] - 1:self.rawData.shape[1]], return_counts=True)
+        print(np.asarray((unique, counts)).T)
